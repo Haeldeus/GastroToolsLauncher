f3b0714eea4bb492ed370f24bdc6ba2b71298686
Haeldeus <nordein.vail@yahoo.de>
2020-08-06 12:21:50 +0200

    First few LoC
    Just the first few LoC for this Project

---
+++ CALauncher/src/launcher/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The Launcher for the CashAssets Application. 
+ * This will check for a new Version of the Launcher itself and asks, if the User wants to do an 
+ * update.
+ * @author Haeldeus
+ * @version 1.0
+ */
+package launcher;
