59f8f906908ca8a5a18e2f000aa1accd974c314f
Haeldeus <nordein.vail@yahoo.de>
2020-09-04 20:59:31 +0200

    Some Testing
    - List added to check Repos, that can be included in the Launcher
    - Just some simple Additions to check the List

--- CALauncher/src/tasks/ProgressTask.java
+++ CALauncher/src/tasks/ProgressTask.java
@@ -1,14 +1,37 @@
 package tasks;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
+import javafx.application.Platform;
 import javafx.concurrent.Task;
+import javafx.scene.control.Label;
+import launcher.WTLauncher;
 
 public class ProgressTask extends Task<Void> {
+  
+  private Label updates;
+  
+  private WTLauncher primary;
+  
+  private int index;
+  
+  private int max;
+  
+  private ArrayList<String> publishedRepos;
+  
+  private ArrayList<String> repoNames;
+  
+  private boolean success;
+  
+  public ProgressTask(Label updates, WTLauncher primary) {
+    this.updates = updates;
+    this.primary = primary;
+    index = 1;
+    max = 100;
+  }
+  
   @Override
-  protected Void call() {
+  protected Void call() {    
     for (int i = 0; i <= 10000; i++) {
       try {
         Thread.sleep(1);
@@ -18,6 +41,7 @@ public class ProgressTask extends Task<Void> {
       }
       updateProgress(i, 10000);
     }
+    /*
     File f = new File(System.getProperty("user.dir") + "/WeyherCalculator.jar");
     // Run a java application in a separate system process
     Process proc;
@@ -30,6 +54,39 @@ public class ProgressTask extends Task<Void> {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    */
     return null;
+  }
+  
+  /**
+   * Updates the ProgressIndicator with the given value and Text to be displayed.
+   * @param value The Value, the Indicator will be updated with. The Percentage displayed will be 
+   *      {@code (value/max)}%
+   * @param text  The Text, that will be displayed in the updates-Label below the Indicator.
+   * @see #max
+   * @see #updates
+   * @since 1.0
+   */
+  protected void updateIndicator(int value, String text) {
+    index = value;
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+        updates.setText(text);
+        updateProgress(value, max);
+      }      
+    });
+  }
+  
+  protected void setResult(boolean success) {
+    this.success = success;
+  }
+  
+  protected void setPublishedRepos(ArrayList<String> repos) {
+    this.publishedRepos = repos;
+  }
+  
+  protected void setRepoNames(ArrayList<String> repoNames) {
+    this.repoNames = repoNames;
   }
 }
